cdea9aa3f68c3d19559ee267d870e10f88a297cf
ritikakhare <ritika@talentsonar.com>
2020-04-26 15:30:24 -0700

    Reddit first commit

--- src/container/Reddit.js
+++ src/container/Reddit.js
@@ -62,3 +62,4 @@ const mapDispatchToProps= dispatch=>{
 
 export default connect(mapStateToProps,mapDispatchToProps)(Reddit);
 
+
--- src/index.js
+++ src/index.js
@@ -18,3 +18,4 @@ ReactDOM.render(
 );
 
 serviceWorker.unregister();
+
