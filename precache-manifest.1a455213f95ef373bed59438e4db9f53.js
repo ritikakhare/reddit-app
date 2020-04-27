96a6fa89d32fdb22db9c6b3c61f2760df4b290a0
ritikakhare <ritika@talentsonar.com>
2020-04-26 17:09:19 -0700

    Updates

---
+++ precache-manifest.1a455213f95ef373bed59438e4db9f53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fdab79b8dd5f32d44f2f048e62aecc9",
+    "url": "/reddit-app/index.html"
+  },
+  {
+    "revision": "b982c737389db34fe65a",
+    "url": "/reddit-app/static/css/main.b4707c96.chunk.css"
+  },
+  {
+    "revision": "f3136e5b83c8ab32d02e",
+    "url": "/reddit-app/static/js/2.d11dc071.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reddit-app/static/js/2.d11dc071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b982c737389db34fe65a",
+    "url": "/reddit-app/static/js/main.168678b2.chunk.js"
+  },
+  {
+    "revision": "6b34a4bface0504fee52",
+    "url": "/reddit-app/static/js/runtime-main.5a4d97c3.js"
+  }
+]);
